e2cb934b0b2b9602e1255a6c4a29716ff21b5e3f
Charity Nyanchera <charitynyancherahh@gmail.com>
2020-05-31 13:53:21 +0300

    Initial commit

---
+++ Profile/Profile/Font/Icons.cs
@@ -0,0 +1,21 @@
+﻿namespace Profile.Font
+{
+    class Icons
+    {
+        
+        public static string youTube = "\uea9d";
+        public const string googlePlus = "\uea8b";
+        public const string menu = "\ue9bd";
+        public const string cog = "\ue994";
+        public const string linkedIn = "\ueaca";
+        public const string Twitter = "\uea96";
+        public const string FaceBook = "\uea9d";
+        public const string WhatsApp = "\uea93";
+        public const string Insta = "\uea92";
+        public const string Mail = "\uea84";
+        public const string Phone = "\ue942";
+        public const string Sphere = "\ue9c9";
+        public const string Flickr = "\ueaa4";
+
+    }
+}
